5edaab0531121ae5f84d12d607ee06666db7c245
chenmengdi <1599126082@qq.com>
2018-01-24 16:27:46 +0800

    Initial Commit

---
+++ 进度/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  进度
+//
+//  Created by 陈孟迪 on 2018/1/24.
+//  Copyright © 2018年 陈孟迪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 进度/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  进度
+//
+//  Created by 陈孟迪 on 2018/1/24.
+//  Copyright © 2018年 陈孟迪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
